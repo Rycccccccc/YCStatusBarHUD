b2795fee66a5f6497992945b83277641ec4800f3
renyichun <renyichun@zcgames.cn>
2016-12-19 10:02:22 +0800

    1.0 开发完毕

--- YCStatusBarHUDDemo/YCStatusBarHUDDemo/ViewController.m
+++ YCStatusBarHUDDemo/YCStatusBarHUDDemo/ViewController.m
@@ -16,7 +16,9 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    // 1. 1.0 完毕
+    
 }
 
 - (IBAction)message:(id)sender {
